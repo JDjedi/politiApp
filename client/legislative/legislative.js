7d204471366c2e85bae0f308c8421984f4464fdc
JDjedi <jcdiaz1201@gmail.com>
2018-04-17 20:38:31 -0500

    Made API twitter calls happen automatically after the page loads, removed api call buttons

--- client/legislative/legislative.js
+++ client/legislative/legislative.js
@@ -28,61 +28,109 @@ if (Meteor.isClient) {
 		}
 	});
 
-	Template.tweetAppRequestSenMinLdr.events({
-		'click #find-a-tweet-senMinLdr': function(){
-			Meteor.call("tweetNow", 'SenSchumer', function(error, response) {
-		  	if (error) {
-		  		alert('Error');
-		  	} else {
-		  		Session.set('senMinLdr', response);
-		  		// console.log(response[0].name);
-		  		return response;
-				}
-			});
-		}
-	});
+	Template.tweetAppRequestSenMajLdr.onRendered(function(){
+		Meteor.call("tweetNow", 'SenateMajLdr', function(error, response) {
+	  	if (error) {
+	  		alert('Error');
+	  	} else {
+	  		Session.set('senMajLdr', response);
+	  		// console.log(response[0].name);
+	  		return response;
+			}
+		});
+	})
 
-	Template.tweetAppRequestSenMajLdr.events({
-		'click #find-a-tweet-senMajLdr': function(){
-			Meteor.call("tweetNow", 'SenateMajLdr', function(error, response) {
-		  	if (error) {
-		  		alert('Error');
-		  	} else {
-		  		Session.set('senMajLdr', response);
-		  		// console.log(response[0].name);
-		  		return response;
-				}
-			});
-		}
-	});
+	Template.tweetAppRequestSenMinLdr.onRendered(function(){
+		Meteor.call("tweetNow", 'SenSchumer', function(error, response) {
+	  	if (error) {
+	  		alert('Error');
+	  	} else {
+	  		Session.set('senMinLdr', response);
+	  		// console.log(response[0].name);
+	  		return response;
+			}
+		});
+	})
 
-	Template.tweetAppRequestHouseMajLdr.events({
-		'click #find-a-tweet-houseMajLdr': function(){
-			Meteor.call("tweetNow", 'SpeakerRyan', function(error, response) {
-		  	if (error) {
-		  		alert('Error');
-		  	} else {
-		  		Session.set('houseMajLdr', response);
-		  		// console.log(response[0].name);
-		  		return response;
-				}
-			});
-		}
-	});
+	Template.tweetAppRequestHouseMajLdr.onRendered(function(){
+		Meteor.call("tweetNow", 'SpeakerRyan', function(error, response) {
+	  	if (error) {
+	  		alert('Error');
+	  	} else {
+	  		Session.set('houseMajLdr', response);
+	  		// console.log(response[0].name);
+	  		return response;
+			}
+		});
+	})
 
-	Template.tweetAppRequestHouseMinLdr.events({
-		'click #find-a-tweet-houseMinLdr': function(){
-			Meteor.call("tweetNow", 'NancyPelosi', function(error, response) {
-		  	if (error) {
-		  		alert('Error');
-		  	} else {
-		  		Session.set('houseMinLdr', response);
-		  		// console.log(response[0].name);
-		  		return response;
-				}
-			});
-		}
-	});
+	Template.tweetAppRequestHouseMinLdr.onRendered(function(){
+		Meteor.call("tweetNow", 'NancyPelosi', function(error, response) {
+	  	if (error) {
+	  		alert('Error');
+	  	} else {
+	  		Session.set('houseMinLdr', response);
+	  		// console.log(response[0].name);
+	  		return response;
+			}
+		});
+	})
+
+	// Template.tweetAppRequestSenMinLdr.events({
+	// 	'click #find-a-tweet-senMinLdr': function(){
+	// 		Meteor.call("tweetNow", 'SenSchumer', function(error, response) {
+	// 	  	if (error) {
+	// 	  		alert('Error');
+	// 	  	} else {
+	// 	  		Session.set('senMinLdr', response);
+	// 	  		// console.log(response[0].name);
+	// 	  		return response;
+	// 			}
+	// 		});
+	// 	}
+	// });
+
+	// Template.tweetAppRequestSenMajLdr.events({
+	// 	'click #find-a-tweet-senMajLdr': function(){
+	// 		Meteor.call("tweetNow", 'SenateMajLdr', function(error, response) {
+	// 	  	if (error) {
+	// 	  		alert('Error');
+	// 	  	} else {
+	// 	  		Session.set('senMajLdr', response);
+	// 	  		// console.log(response[0].name);
+	// 	  		return response;
+	// 			}
+	// 		});
+	// 	}
+	// });
+
+	// Template.tweetAppRequestHouseMajLdr.events({
+	// 	'click #find-a-tweet-houseMajLdr': function(){
+	// 		Meteor.call("tweetNow", 'SpeakerRyan', function(error, response) {
+	// 	  	if (error) {
+	// 	  		alert('Error');
+	// 	  	} else {
+	// 	  		Session.set('houseMajLdr', response);
+	// 	  		// console.log(response[0].name);
+	// 	  		return response;
+	// 			}
+	// 		});
+	// 	}
+	// });
+
+	// Template.tweetAppRequestHouseMinLdr.events({
+	// 	'click #find-a-tweet-houseMinLdr': function(){
+	// 		Meteor.call("tweetNow", 'NancyPelosi', function(error, response) {
+	// 	  	if (error) {
+	// 	  		alert('Error');
+	// 	  	} else {
+	// 	  		Session.set('houseMinLdr', response);
+	// 	  		// console.log(response[0].name);
+	// 	  		return response;
+	// 			}
+	// 		});
+	// 	}
+	// });
 }
 
 
--- client/main/index.js
+++ client/main/index.js
@@ -11,40 +11,64 @@ if (Meteor.isClient) {
 		}
 	});
 
-	Template.tweetAppRequestPresident.events({
-		'click #find-a-tweet-president': function(){
-			Meteor.call("tweetNow", 'realDonaldTrump', function(error, response) {
-		  	if (error) {
-		  		alert('Error');
-		  	} else {
-		  		Session.set('president', response);
-		  		// console.log(response[0].name);
-		  		return response;
-				}
-			});
-		}
-	});
-
 	Template.tweetAppRequestSCOTUS.helpers({
 		supCourt: function() {
 			return Session.get('supCourt');
 		}
 	});
 
-	Template.tweetAppRequestSCOTUS.events({
-		'click #find-a-tweet-sc': function(){
-			Meteor.call("tweetNow", 'USSupremeCourt', function(error, response) {
-		  	if (error) {
-		  		alert('Error');
-		  	} else {
-		  		Session.set('supCourt', response);
-		  		// console.log(response[0].name);
-		  		return response;
-				}
-			});
-		}
-	});
+	// Template.tweetAppRequestPresident.events({ //old method call, with button
+	// 	'click #find-a-tweet-president': function(){
+	// 		Meteor.call("tweetNow", 'realDonaldTrump', function(error, response) {
+	// 	  	if (error) {
+	// 	  		alert('Error');
+	// 	  	} else {
+	// 	  		Session.set('president', response);
+	// 	  		// console.log(response[0].name);
+	// 	  		return response;
+	// 			}
+	// 		});
+	// 	}
+	// });
 
+	Template.tweetAppRequestPresident.onRendered(function(){
+		Meteor.call("tweetNow", 'realDonaldTrump', function(error, response) {
+	  	if (error) {
+	  		alert('Error');
+	  	} else {
+	  		Session.set('president', response);
+	  		// console.log(response[0].name);
+	  		return response;
+			}
+		});
+	})
+
+	Template.tweetAppRequestSCOTUS.onRendered(function(){
+		Meteor.call("tweetNow", 'USSupremeCourt', function(error, response) {
+	  	if (error) {
+	  		alert('Error');
+	  	} else {
+	  		Session.set('supCourt', response);
+	  		// console.log(response[0].name);
+	  		return response;
+			}
+		});
+	})
+
+
+	// Template.tweetAppRequestSCOTUS.events({
+	// 	'click #find-a-tweet-sc': function(){
+	// 		Meteor.call("tweetNow", 'USSupremeCourt', function(error, response) {
+	// 	  	if (error) {
+	// 	  		alert('Error');
+	// 	  	} else {
+	// 	  		Session.set('supCourt', response);
+	// 	  		// console.log(response[0].name);
+	// 	  		return response;
+	// 			}
+	// 		});
+	// 	}
+	// });
 }
 
 
