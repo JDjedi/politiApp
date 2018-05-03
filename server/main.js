caf4666e317f65c780f1bf036a7e090869dda68e
Jonathan Diaz <jcdiaz1201@gmail.com>
2018-05-03 17:06:34 -0500

    Update main.js

--- server/main.js
+++ server/main.js
@@ -17,13 +17,7 @@ Meteor.startup(() => {
 			var Twit = require('twit');
 			var future = new Future();
 
-			var T = new Twit({
-			  consumer_key:         'Sse8REMzb6VNZvcfGT0Z3T5F2',
-			  consumer_secret:      'iS4rxN1QlFTEy8FZlQj13dj6x8M6ASDYYLLCDiYUgxPz0gX8tQ',
-			  access_token:         '510936252-iKweDgvqdSmNZMOEmVwcdXlzQlVkZNl7ShQzefE5',
-			  access_token_secret:  'zLDjI7NcXUs6UyTWucMZVczyDV4PP2sZzzbvok9a0QNW8',
-			  timeout_ms:           60*1000,  // optional HTTP request timeout to apply to all requests.
-			});
+
 
 			T.get('users/lookup', { screen_name: publicFigure, tweet_mode: 'extended' }, function(err, data, response) {
 				if (err) {
